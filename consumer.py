8af8038a1cef6ece7e1e0e633395922a1bf04883
trung <thaibaotrung0607@gmail.com>
2023-12-09 14:25:38 +0700

    add consumer

---
+++ consumer.py
@@ -0,0 +1,12 @@
+from kafka import KafkaConsumer
+import pandas as pd
+from json import loads
+
+consumer = KafkaConsumer(bootstrap_servers='masternode:9092')
+consumer.subscribe('trungdeptrai')
+for message in consumer:
+    print(message)
+    message = message.value
+    json = loads(message)
+    df = pd.json_normalize(json)
+    df.to_csv("bigdata.csv", mode='a', header=False)
